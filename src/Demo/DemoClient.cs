61b43563a80bacfb2a88e3ca544d039fe0a00c15
dr <dr@dr>
2026-01-17 19:56:45 -0800

    Initial commit for WheelTelnet.

---
+++ src/Demo/DemoClient.cs
@@ -0,0 +1,11 @@
+﻿namespace WheelTelnet.Demo
+{
+    internal static class DemoClient
+    {
+        internal static void Run()
+        {
+            // TODO: Once TelnetServer is solid, also refactor to have a TelnetClient class?
+            //       Then add testing mode for it here!
+        }
+    }
+}
---
+++ src/WheelTelnet/PortInUseException.cs
@@ -0,0 +1,8 @@
+﻿namespace WheelTelnet
+{
+    /// <summary>Exception for the attempted socket port being in use.</summary>
+    /// <param name="message">The exception message.</param>
+    public class PortInUseException(string message) : Exception(message)
+    {
+    }
+}
